ce4df13a68e364118dd9f0f63c08535c154c9104
Yigit.Dev <152687620+yigitdevv0@users.noreply.github.com>
2024-01-18 21:56:37 +0300

    Create tippy.js

---
+++ assets/js/tippy.js
@@ -0,0 +1,11 @@
+tippy('#headAvatar', {
+    content: '<p class="smallText opacity-80 ml-1 mr-1">working for better</p>',
+    animation: 'scale',
+    placement: 'bottom',
+    theme: 'ws',
+    inertia: true,
+    interactive: false,
+    allowHTML: true,
+    followCursor: false,
+    arrow: false
+});
